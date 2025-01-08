ffae6e34c4da9eaaf66fc538774e2be0fedd0637
ronaldo neto <ronaldoneto227@gmail.com>
2025-01-08 20:26:52 -0300

    Commit inicial - Adiciona estrutura de pastas e arquivos do projeto

---
+++ src/br/com/main/CapotariaBatista_ControleEstoque.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+package br.com.main;
+
+/**
+ *
+ * @author ronaldo neto
+ */
+public class CapotariaBatista_ControleEstoque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
