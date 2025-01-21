36cfec1d99dba845d969732d0feec5d39115aaac
ronaldo neto <ronaldoneto227@gmail.com>
2025-01-21 05:32:16 -0300

    feat: implementa controlador para login com validação e mensagem de alerta

--- src/br/com/controller/LoginController.java
+++ src/br/com/controller/LoginController.java
@@ -1,20 +1,53 @@
 package br.com.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import br.com.service.LoginService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 
 /**
  * FXML Controller class
  *
  * @author ronaldo neto
  */
-public class LoginController implements Initializable {
+public class LoginController {
 
+    @FXML
+    private TextField UsernameField;
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-      
-    }    
+    @FXML
+    private PasswordField PasswordField;
     
+    private final LoginService loginService = new LoginService();
+     
+    @FXML
+     private void handleLogin(ActionEvent event){
+            String username = UsernameField.getText();
+            String senha = PasswordField.getText();
+            
+            if(username.isEmpty() || senha.isEmpty()){
+                showAlert(AlertType.WARNING, "Aviso", "Preencha todos os campos!");
+                return;
+            }
+            
+            boolean isAuthenticadet = loginService.authenticate(username, senha);
+            
+            if(isAuthenticadet){
+                showAlert(AlertType.INFORMATION, "Sucesso", "Login realizado com sucesso");
+            }else{
+                showAlert(AlertType.ERROR, "Erro", "Usuário ou senha inválidos");
+            }
+    }         
+            
+     
+     private void showAlert(AlertType alertType, String title, String content){
+         Alert alert = new Alert(alertType);
+         alert.setTitle(title);
+         alert.setContentText(content);
+         alert.showAndWait();
+     }
 }
