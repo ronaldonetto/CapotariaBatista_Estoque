6d6aa739b3aadce9b66992bdb51b3f657614964d
ronaldo neto <ronaldoneto227@gmail.com>
2025-01-28 19:59:19 -0300

    Iniciada implementação da classe ProdutoDAO

---
+++ src/br/com/dao/ProdutoDAO.java
@@ -0,0 +1,26 @@
+package br.com.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import br.com.model.Produto;
+import br.com.dao.ConnectionDataBase;
+import java.sql.Date;
+
+
+/**
+ *
+ * @author ronaldo neto
+ */
+
+public class ProdutoDAO {
+    
+ 
+    }
+    
+
