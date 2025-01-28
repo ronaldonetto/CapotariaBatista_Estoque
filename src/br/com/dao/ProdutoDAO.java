f7ab5eff538e31d487bd8402c130e12e18c0f6d8
ronaldo neto <ronaldoneto227@gmail.com>
2025-01-28 20:36:12 -0300

    Criado método salvar para inserir dados na tabela produtos no banco de dados

--- src/br/com/dao/ProdutoDAO.java
+++ src/br/com/dao/ProdutoDAO.java
@@ -20,7 +20,24 @@ import java.sql.Date;
 
 public class ProdutoDAO {
     
+    //Método para salvar os produtos no banco de dados. Utilizado queries 
+    public void salvar(Produto produto) throws SQLException {
+        String sql = "INSERT INTO Produtos (os_produto, nome_produto, categoria, quantidade_metro, quantidade, descricao, fornecedor, data_cadastro)  VALUES (?,?,?,?,?,?,?,?)";     
+        try (Connection conn = ConnectionDataBase.conectar(); 
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            
+            stmt.setInt(1, produto.getOs());
+            stmt.setString(2, produto.getProduto());
+            stmt.setString(3, produto.getCategoria());
+            stmt.setDouble(4, produto.getMetragem());
+            stmt.setInt(5, produto.getQuantidade());
+            stmt.setString(6, produto.getDescricao());
+            stmt.setString(7, produto.getFornecedor());
+            stmt.setDate(8, Date.valueOf(produto.getDataCadastro()));
+            
+            stmt.executeUpdate();
+        }
  
     }
-    
+}    
 
